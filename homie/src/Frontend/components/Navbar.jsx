09d463b06c129b3a5de64c1c1f77e32e82a9bdc8
Gautam Adatra <gautamkiran123@gmail.com>
2023-06-24 15:00:02 +0530

    create react app

---
+++ homie/src/Frontend/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Navbar = () => {
+    return (
+        <nav>
+            <div>Home</div>
+            <div>About</div>
+            <div>The devs</div>
+            <div>homie</div>
+            <button>Login</button>
+            <button>Signup</button>
+        </nav>
+    )
+}
