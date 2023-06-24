a0df529e51dfbd3341752b7bb8264ad63a459765
Gautam Adatra <gautamkiran123@gmail.com>
2023-06-24 16:58:32 +0530

    homepage first draft

--- homie/src/Frontend/components/Navbar.jsx
+++ homie/src/Frontend/components/Navbar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-
+import "../components/Navbar.module.css";
 export const Navbar = () => {
     return (
-        <nav>
-            <div>Home</div>
-            <div>About</div>
-            <div>The devs</div>
-            <div>homie</div>
-            <button>Login</button>
-            <button>Signup</button>
+        <nav className="navBox">
+            <div>
+            <h2 style={{
+                fontSize:'2.25rem',
+                margin:'auto'
+            }}>homie</h2>
+            </div>
+            <div className="navlinks">
+            <h3>Home</h3>
+            <h3>About</h3>
+            <h3>The designers</h3>
+            <h3>The devs</h3>
+            </div>
+            
+            <div className="btnBox">
+                <button className="btnPrimary">Login</button>
+                <button className="btnSecondary">Signup</button>
+            </div>
         </nav>
     )
 }
